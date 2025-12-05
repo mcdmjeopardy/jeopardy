66f0b190bed886a7b213e7e051fece94059f957e
RasmusLeddyr <rasmusleddyr@hotmail.com>
2025-12-05 09:28:32 +0100

    setStyles function polish

--- src/functions/setStyles.jsx
+++ src/functions/setStyles.jsx
@@ -1,16 +1,11 @@
 export function cx(styles, moduleClasslist, globalClasslist) {
-  let mc = moduleClasslist
-    .trim()
+  const mc = moduleClasslist
     .split(/\s+/)
     .map((i) => styles[i])
     .filter(Boolean)
     .join(" ");
 
-  let gc = globalClasslist.trim();
+  const classlist = mc + " " + globalClasslist.trim();
 
-  let classlist = mc + " " + gc;
-  classlist = classlist.trim();
   return classlist;
 }
-
-// figure out optimisations with white space removal!
