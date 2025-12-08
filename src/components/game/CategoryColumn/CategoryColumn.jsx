e1b14b2fbd87ff017f140c315c4481cb9332e3ac
RasmusLeddyr <rasmusleddyr@hotmail.com>
2025-12-08 09:20:00 +0100

    removed console log

--- src/components/game/CategoryColumn/CategoryColumn.jsx
+++ src/components/game/CategoryColumn/CategoryColumn.jsx
@@ -2,7 +2,6 @@ import { cn } from "../../../functions/setStyles";
 import styles from "./CategoryColumn.module.css";
 
 const CategoryColumn = ({ gs }) => {
-  console.log(gs);
   return (
     <div className={cn(styles, `module1 module2`, `${gs} global1 global2`)}>
       CategoryColumn
