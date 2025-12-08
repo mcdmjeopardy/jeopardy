33fdb13298074692498792452542d0b7cc86b912
RasmusLeddyr <rasmusleddyr@hotmail.com>
2025-12-08 09:45:58 +0100

    question tile

--- src/components/game/GameBoard/GameBoard.jsx
+++ src/components/game/GameBoard/GameBoard.jsx
@@ -1,5 +1,6 @@
 import Button from "../../common/Button/Button";
 import CategoryColumn from "../CategoryColumn/CategoryColumn";
+import QuestionTile from "../QuestionTile/QuestionTile";
 import styles from "./GameBoard.module.css";
 
 const GameBoard = () => {
@@ -8,6 +9,7 @@ const GameBoard = () => {
       GameBoard
       <Button content="Test button" />
       <CategoryColumn gs="test1 test2" />
+      <QuestionTile />
     </div>
   );
 };
