e85582101881b9aaf25d911518fc5beaf857831e
Jannik Laursen <jannik@laursen.ceo>
2025-12-08 11:21:34 +0100

    default styling

--- src/components/game/GameBoard/GameBoard.jsx
+++ src/components/game/GameBoard/GameBoard.jsx
@@ -6,9 +6,18 @@ import styles from "./GameBoard.module.css";
 const GameBoard = () => {
   return (
     <div className={cn(styles, `container`)}>
-      GameBoard
-      <Button content="Test button" />
-      <CategoryColumn gs="test1 test2" />
+      <header>
+        <h1> GameBoard</h1>
+      </header>
+      <aside>
+        <h2>subtitle</h2>
+        <Button content="Click me" />
+      </aside>
+      <main>
+        <h2>subtitle</h2>
+        <CategoryColumn gs="test1 test2" />
+        <Button content="Test button" />
+      </main>
     </div>
   );
 };
