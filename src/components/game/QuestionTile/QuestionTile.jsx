ac07dcc8f9b90dc21871162acd01a2f9d050efde
Fluee13 <emilsp05@gmail.com>
2025-12-09 12:05:01 +0100

    Moved tilesStyling from global to module

--- src/components/game/QuestionTile/QuestionTile.jsx
+++ src/components/game/QuestionTile/QuestionTile.jsx
@@ -3,9 +3,7 @@ import styles from "./QuestionTile.module.css";
 import { cn } from "../../../functions/setStyles";
 
 const QuestionTile = ({ ms }) => {
-  return (
-    <div className={cn(styles, `${ms} container`, `tile`)}>100</div>
-  );
+  return <div className={cn(styles, `${ms} container tile`, ``)}>100</div>;
 };
 
 export default QuestionTile;
