ca7c171d84715b1e7ea5c319241fb15c97fc135e
Fluee13 <emilsp05@gmail.com>
2025-12-09 12:57:40 +0100

    tiles_fixes

--- src/components/game/GameBoard/GameBoard.jsx
+++ src/components/game/GameBoard/GameBoard.jsx
@@ -1,5 +1,4 @@
 import { cn } from "../../../functions/setStyles";
-import Button from "../../common/Button/Button";
 import CategoryColumn from "../CategoryColumn/CategoryColumn";
 import styles from "./GameBoard.module.css";
 
@@ -7,14 +6,9 @@ const GameBoard = () => {
   return (
     <div className={cn(styles, ``, "container")}>
       <header>
-        <h1> GameBoard</h1>
+        <h1> Jeopardy</h1>
       </header>
-      <aside>
-        <h2>subtitle</h2>
-        <Button content="Click me" />
-      </aside>
       <main>
-        <h2>subtitle</h2>
         <div className={cn(styles, `columns`)}>
           {/* Temporary manual setup. It's supposed to generate a column per category from API. */}
           <CategoryColumn title="Sange" />
@@ -23,7 +17,6 @@ const GameBoard = () => {
           <CategoryColumn title="Julemand" />
           <CategoryColumn title="Diverse" />
         </div>
-        <Button content="Test button" />
       </main>
     </div>
   );
