91b1cb80b73a53ea73bba57beae5fbd73ddcc075
Fluee13 <emilsp05@gmail.com>
2025-12-10 11:19:36 +0100

    ScoreBoard/columns

--- src/components/game/Scoreboard/ScoreBoard.jsx
+++ src/components/game/Scoreboard/ScoreBoard.jsx
@@ -1,11 +1,29 @@
 import { cn } from "../../../functions/setStyles";
 import julmand from "../../../assets/julmand.jpg";
+import icebear from "../../../assets/icebear.jpg";
+import julemandven from "../../../assets/julemand-ven.jpg";
+import juletræ from "../../../assets/juletræ.jpg";
 import styles from "./ScoreBoard.module.css";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { MedalFirstPlaceIcon } from "@hugeicons/core-free-icons";
+import { MedalSecondPlaceIcon } from "@hugeicons/core-free-icons";
+import { MedalThirdPlaceIcon } from "@hugeicons/core-free-icons";
+import { CryingIcon } from "@hugeicons/core-free-icons";
+
 
 const ScoreBoard = () => {
   return (
     <div className={cn(styles, `container`, "")}>
       <div className={cn(styles, `teamcard`)}>
+        <img src={juletræ} className={cn(styles, `avatar`)}></img>
+        <div className={cn(styles, `icon`)}>
+          <HugeiconsIcon
+            icon={MedalFirstPlaceIcon}
+            size={40}
+            color="#f0c435"
+            strokeWidth={1.5}
+          />
+        </div>
         <div className={cn(styles, `names`)}>
           {" "}
           <h3>Team 1</h3>{" "}
@@ -16,6 +34,16 @@ const ScoreBoard = () => {
       </div>
       <div className={cn(styles, `teamcard`)}>
         <img src={julmand} className={cn(styles, `avatar`)}></img>
+        <div className={cn(styles, `icon`)}>
+          <div className={cn(styles, `icon`)}>
+            <HugeiconsIcon
+              icon={MedalSecondPlaceIcon}
+              size={40}
+              color="#bababaff"
+              strokeWidth={1.5}
+            />
+          </div>
+        </div>
         <div className={cn(styles, `names`)}>
           {" "}
           <h3>Team 2</h3>
@@ -25,6 +53,15 @@ const ScoreBoard = () => {
         </div>
       </div>
       <div className={cn(styles, `teamcard`)}>
+        <img src={icebear} className={cn(styles, `avatar`)}></img>
+        <div className={cn(styles, `icon`)}>
+          <HugeiconsIcon
+            icon={MedalThirdPlaceIcon}
+            size={40}
+            color="#cc7d16ff"
+            strokeWidth={1.5}
+          />
+        </div>
         <div className={cn(styles, `names`)}>
           <h3>Team 3</h3>
         </div>
@@ -33,6 +70,15 @@ const ScoreBoard = () => {
         </div>
       </div>
       <div className={cn(styles, `teamcard`)}>
+        <img src={julemandven} className={cn(styles, `avatar`)}></img>
+        <div className={cn(styles, `Cryingicon`)}>
+          <HugeiconsIcon
+            icon={CryingIcon}
+            size={24}
+            color="#fa0a1a"
+            strokeWidth={1.5}
+          />
+        </div>
         <div className={cn(styles, `names`)}>
           <h3>Team 4</h3>
         </div>
