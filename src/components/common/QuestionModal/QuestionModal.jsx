d700cf08770707fababed082ef6b11d57918ede8
RasmusLeddyr <rasmusleddyr@hotmail.com>
2025-12-12 11:46:43 +0100

    question-modal animation done

--- src/components/common/QuestionModal/QuestionModal.jsx
+++ src/components/common/QuestionModal/QuestionModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import styles from "./QuestionModal.module.css";
 import { cn } from "../../../functions/setStyles";
 import Button from "../Button/Button";
@@ -9,8 +9,33 @@ const QuestionModal = ({ question, closeModal }) => {
     setAnswerShown(!isAnswerShown);
   };
 
+  const [shiftY, setShiftY] = useState(0);
+
+  const refQuestion = useRef(null);
+  const refLine = useRef(null);
+
+  useLayoutEffect(() => {
+    const getHeight = (elmt) => {
+      if (!elmt) return 0;
+      const css = window.getComputedStyle(elmt);
+      const mt = parseFloat(css.marginTop);
+      const mb = parseFloat(css.marginBottom);
+      const rect = elmt.getBoundingClientRect();
+      return rect.height + mt + mb;
+    };
+    const combineHeights = () => {
+      const hQuestion = getHeight(refQuestion.current);
+      const hLine = getHeight(refLine.current);
+      setShiftY(hQuestion / 2 + hLine / 2);
+    };
+
+    combineHeights();
+  }, []);
+
   return (
-    <main className={cn(styles, `${!isAnswerShown && "answerHidden"} container`)}>
+    <main
+      className={cn(styles, `${!isAnswerShown && "answerHidden"} container`)}
+    >
       <div className={cn(styles, "top")}>
         <div className={cn(styles, "counter")}>100</div>
         <div className={styles.btns}>
@@ -24,8 +49,14 @@ const QuestionModal = ({ question, closeModal }) => {
       </div>
 
       <div className={cn(styles, "center")}>
-        <div className={cn(styles, "question")}>Question</div>
-        <div className={cn(styles, "line")} />
+        <div
+          ref={refQuestion}
+          style={{ transform: `translateY(${isAnswerShown ? 0 : shiftY}px)` }}
+          className={cn(styles, "question")}
+        >
+          Question
+        </div>
+        <div ref={refLine} className={cn(styles, "line")} />
         <div className={cn(styles, "answer")}>Answer</div>
       </div>
     </main>
