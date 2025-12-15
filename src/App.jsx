fe2c6776e8e2f2ca12878124298c63131b11b4b7
Jannik Laursen <jannik@laursen.ceo>
2025-12-15 02:58:52 +0100

    update api and context files

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GameCreator from "./components/admin/GameCreator/GameCreator";
 import GameLobby from "./components/admin/GameLobby/GameLobby";
-import GameBoard from "./components/game/GameBoard/GameBoard";
 import EditProfile from "./components/admin/TeamEdit/TeamEdit";
-
+import GameBoard from "./components/game/GameBoard/GameBoard";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app-container">
         <Routes>
-          <Route path="/" element={<GameBoard />} />
-          <Route path="/admin" element={<GameCreator />} />
-          <Route path="/lobby" element={<GameLobby />} />
+          <Route path="/game" element={<GameBoard />} />
+          <Route path="/newgame" element={<GameCreator />} />
+          <Route path="/" element={<GameLobby />} />
           <Route path="/edit" element={<EditProfile />} />
         </Routes>
       </div>
