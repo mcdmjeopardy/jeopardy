695f28de25ad5ec04c826c6c2b9e04d3a4ed2083
Jannik Laursen <jannik@laursen.ceo>
2025-12-16 09:09:43 +0100

    fixed app.jsx routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AdminPanel from "./components/admin/AdminPanel/AdminPanel";
 import GameLobby from "./components/admin/GameLobby/GameLobby";
 import EditProfile from "./components/admin/TeamEdit/TeamEdit";
+import GameBoard from "./components/game/GameBoard/GameBoard";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app-container">
         <Routes>
-          <Route path="/" element={<GameBoard />} />
+          <Route path="/game" element={<GameBoard />} />
           <Route path="/admin" element={<AdminPanel />} />
-          <Route path="/lobby" element={<GameLobby />} />
+          <Route path="/" element={<GameLobby />} />
           <Route path="/edit" element={<EditProfile />} />
         </Routes>
       </div>
